2c4776e1fb9632688cc459053beaaea387010e1d
kamil <kamil.malita@gmail.com>
2020-10-10 19:35:28 +0200

    Added @Repository annotations

--- src/main/java/com/library/govLibrary/repository/LibraryRepository.java
+++ src/main/java/com/library/govLibrary/repository/LibraryRepository.java
@@ -2,6 +2,8 @@ package com.library.govLibrary.repository;
 
 import com.library.govLibrary.model.Library;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LibraryRepository extends JpaRepository<Library, Long> {
 }
