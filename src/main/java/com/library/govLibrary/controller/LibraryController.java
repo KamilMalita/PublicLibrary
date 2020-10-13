531dec0e027a4a38f0c394ca91fa19a2fc64d9f9
kamil <kamil.malita@gmail.com>
2020-10-13 17:12:20 +0200

    Add new GetLibrarys Endpoint

--- src/main/java/com/library/govLibrary/controller/LibraryController.java
+++ src/main/java/com/library/govLibrary/controller/LibraryController.java
@@ -5,7 +5,10 @@ import com.library.govLibrary.service.LibraryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,4 +19,7 @@ public class LibraryController {
     public Library getById(@PathVariable long id){
         return libraryService.getById(id);
     }
+
+    @GetMapping("/librarys")
+    public List<Library> getLibrarys(@RequestParam(required = false) int page) { return libraryService.getLibrarys(page); }
 }
