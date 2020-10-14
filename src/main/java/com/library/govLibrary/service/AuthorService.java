0044ffeb045d814b276d6bbae3090b3b7f3dcd57
kamil <kamil.malita@gmail.com>
2020-10-14 17:42:38 +0200

    Correct AuthorService

--- src/main/java/com/library/govLibrary/service/AuthorService.java
+++ src/main/java/com/library/govLibrary/service/AuthorService.java
@@ -25,14 +25,10 @@ public class AuthorService {
 
     public List<Author> getAuthorsWithBooks(int page){
         List<Author> authorList = authorRepository.findAllAuthors(PageRequest.of(page, SIZE));
-        List<Long> listAuthorId = authorList.stream().map(author -> author.getId()).collect(Collectors.toList());
+        List<Long> listAuthorId = authorList.stream().map(Author::getId).collect(Collectors.toList());
         List<Book> bookList = bookRepository.findAllByAuthorIdIn(listAuthorId);
         authorList.forEach(author -> author.setBook(extractBooks(bookList, author.getId())));
         return authorList;
-    }
-
-    private List<Book> extractBooks(List<Book> bookList, long id) {
-        return bookList.stream().filter(book -> book.getAuthorId() == id).collect(Collectors.toList());
     }
 
     public List<Author> getAuthorsWithSurname(String surname){
@@ -45,5 +41,9 @@ public class AuthorService {
 
     public Author getAuthor(long id) {
         return authorRepository.findById(id).orElseThrow();
+    }
+
+    private List<Book> extractBooks(List<Book> bookList, long id) {
+        return bookList.stream().filter(book -> book.getAuthorId() == id).collect(Collectors.toList());
     }
 }
