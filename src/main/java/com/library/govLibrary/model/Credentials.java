f01d34cf4809ca26006d713422ae5d7b390a802e
kamil <kamil.malita@gmail.com>
2020-10-22 18:21:03 +0200

    Add Getters, all in String format

--- src/main/java/com/library/govLibrary/model/Credentials.java
+++ src/main/java/com/library/govLibrary/model/Credentials.java
@@ -2,8 +2,15 @@ package com.library.govLibrary.model;
 
 import lombok.Getter;
 
-@Getter
 public class Credentials {
     private String username;
     private char[] password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return String.valueOf(password);
+    }
 }
