6ab30bb86065ea2d773c52e3797b2458147db705
kamil.malita@gmail.com <kamil.malita@gmail.com>
2020-10-26 20:07:59 +0100

    Add Register Endpoint

--- src/main/java/com/library/govLibrary/controller/LoginController.java
+++ src/main/java/com/library/govLibrary/controller/LoginController.java
@@ -1,15 +1,25 @@
 package com.library.govLibrary.controller;
 
 import com.library.govLibrary.model.Credentials;
+import com.library.govLibrary.model.Users;
+import com.library.govLibrary.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LoginController {
+    private final UserService userService;
 
     @PostMapping("/login")
-    public void login(@RequestBody Credentials credentials){
+    public void login(@RequestBody Credentials credentials) {
+    }
+
+    @GetMapping("/register")
+    public Users register() {
+        return userService.createUser();
     }
 }
