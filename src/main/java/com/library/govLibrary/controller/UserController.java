ade676fac2382841caed05f646c908eaba9ebfbb
kamil <kamil.malita@gmail.com>
2020-10-30 23:19:04 +0100

    Save local changes

--- src/main/java/com/library/govLibrary/controller/UserController.java
+++ src/main/java/com/library/govLibrary/controller/UserController.java
@@ -7,6 +7,8 @@ import com.library.govLibrary.validation.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,12 @@ public class UserController {
     @PostMapping("/login")
     public void login(@RequestBody Credentials credentials) {
     }
+
+    @InitBinder("register")
+    protected void initBinder(WebDataBinder binder){
+        binder.addValidators(new UserValidator());
+    }
+
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Validated @RequestBody Users user) {
